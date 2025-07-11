07cb859627c425f60d84eaa2cbdf9a97c5040546
Ademir2021 <centroserra@gmail.com>
2025-07-11 16:20:39 -0300

    up-code

--- git_up.sh
+++ git_up.sh
@@ -1,9 +1,4 @@
 #bin/bash
-# echo "# api_js_1.0" >> README.md
-# git init
-# git add README.md
 git add .
 git commit -m "up-code"
-# git branch -M main
-# git remote add origin git@github.com:Ademir2021/api_js_1.0.git
 git push -u origin main
