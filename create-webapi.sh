801a60c58947c3c3ad3055bce4920b92a564d882
Ademir2021 <centroserra@gmail.com>
2025-07-11 17:18:29 -0300

    up-code

--- create-webapi.sh
+++
@@ -1,8 +0,0 @@
-#bin/bash
-clear
-npx tsc
-rm -rf ../../Dockers/webapi/build/
-echo 'Criando nova build para webapi'
-cp -rf build/ ../../Dockers/webapi/build/
-# cp -rf node_modules ../api-centroinfo
-echo 'build webapi gerada com sucesso'
